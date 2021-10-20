8b3b85a0785f06b6a9a11793cff1b0a3059a0a67
Bilinska_Iryna <bilinska.iryna.02@gmail.com>
2021-10-20 18:33:18 +0300

    Solution_01

--- src/validateRegisterForm.test.js
+++ src/validateRegisterForm.test.js
@@ -19,6 +19,13 @@ describe(`Function 'validateRegisterForm':`, () => {
     expect(isValid.message).toBe('Email and password are valid.');
   });
 
+  it(`should return success message for the valid input`, () => {
+    const isValid = validateRegisterForm('test@mail.com', 'P@ssword1!');
+
+    expect(isValid.code).toBe(200);
+    expect(isValid.message).toBe('Email and password are valid.');
+  });
+
   it(`should return error for valid email and password without number`, () => {
     const invalidPassword = validateRegisterForm('test@mail.com', 'P@ssword');
 
@@ -26,5 +33,98 @@ describe(`Function 'validateRegisterForm':`, () => {
     expect(invalidPassword.message).toBe('Password is invalid.');
   });
 
-  // write more tests here
+  it(`should return error for valid password and email without @`, () => {
+    const invalidEmailMask = validateRegisterForm('testmail.com', 'P@ssword1');
+
+    expect(invalidEmailMask.code).toBe(422);
+    expect(invalidEmailMask.message).toBe('Email is invalid.');
+  });
+
+  it(`should return error for valid password and email without @`, () => {
+    const invalidEmailMaskAndPassword = validateRegisterForm(
+      'testmail.com', 'P@ssword');
+
+    expect(invalidEmailMaskAndPassword.code).toBe(500);
+
+    expect(invalidEmailMaskAndPassword.message)
+      .toBe('Password and email are invalid.');
+  });
+
+  it(`should return success message for the valid input`, () => {
+    const isValid = validateRegisterForm('test1@mail.com', 'P@ssword1!123456');
+
+    expect(isValid.code).toBe(200);
+    expect(isValid.message).toBe('Email and password are valid.');
+  });
+
+  it(`should return success message for the valid input`, () => {
+    const invalidPassword = validateRegisterForm('te_st@mail.com', 'P@sd1');
+
+    expect(invalidPassword.code).toBe(422);
+    expect(invalidPassword.message).toBe('Password is invalid.');
+  });
+
+  it(`should return success message for the valid input`, () => {
+    const invalidPassword = validateRegisterForm(
+      'te.st@mail.com', 'P@ssword1!12345678');
+
+    expect(invalidPassword.code).toBe(422);
+    expect(invalidPassword.message).toBe('Password is invalid.');
+  });
+
+  it(`should return success message for the valid input`, () => {
+    const invalidPassword = validateRegisterForm(
+      'test@mail.lom.com', 'p@ssword1!');
+
+    expect(invalidPassword.code).toBe(422);
+    expect(invalidPassword.message).toBe('Password is invalid.');
+  });
+
+  it(`should return success message for the valid input`, () => {
+    const invalidPassword = validateRegisterForm(
+      'test@mail.lo_m.com', 'P@ssword!');
+
+    expect(invalidPassword.code).toBe(422);
+    expect(invalidPassword.message).toBe('Password is invalid.');
+  });
+
+  it(`should return error for valid password and email without @`, () => {
+    const invalidEmailMask = validateRegisterForm(
+      '.test@mail.com', 'P@ssword1');
+
+    expect(invalidEmailMask.code).toBe(422);
+    expect(invalidEmailMask.message).toBe('Email is invalid.');
+  });
+
+  it(`should return error for valid password and email without @`, () => {
+    const invalidEmailMask = validateRegisterForm(
+      'te..st@mail.com', 'P@ssword1');
+
+    expect(invalidEmailMask.code).toBe(422);
+    expect(invalidEmailMask.message).toBe('Email is invalid.');
+  });
+
+  it(`should return error for valid password and email without @`, () => {
+    const invalidEmailMask = validateRegisterForm(
+      'test@.mail.com', 'P@ssword1');
+
+    expect(invalidEmailMask.code).toBe(422);
+    expect(invalidEmailMask.message).toBe('Email is invalid.');
+  });
+
+  it(`should return error for valid password and email without @`, () => {
+    const invalidEmailMask = validateRegisterForm(
+      'te:st@mail.com', 'P@ssword1');
+
+    expect(invalidEmailMask.code).toBe(422);
+    expect(invalidEmailMask.message).toBe('Email is invalid.');
+  });
+
+  it(`should return error for valid password and email without @`, () => {
+    const invalidEmailMask = validateRegisterForm(
+      'test@ma:il.com', 'P@ssword1');
+
+    expect(invalidEmailMask.code).toBe(422);
+    expect(invalidEmailMask.message).toBe('Email is invalid.');
+  });
 });
