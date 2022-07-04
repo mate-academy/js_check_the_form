6350af4e1de65fcf24c2e3620099bd0cc938e690
krolich500grn <edu.izaytsev@gmail.com>
2022-07-04 14:24:31 +0300

    feat: tests for validateRegisterForm func

--- src/validateRegisterForm.test.js
+++ src/validateRegisterForm.test.js
@@ -26,5 +26,97 @@ describe(`Function 'validateRegisterForm':`, () => {
     expect(invalidPassword.message).toBe('Password is invalid.');
   });
 
-  // write more tests here
+  describe(`Email field`, () => {
+    it(`should contain only english letters`, () => {
+      const invalidMail = validateRegisterForm('тест@mail.com', 'P@ssword1!');
+
+      expect(invalidMail.message).toBe('Email is invalid.');
+    });
+
+    it(`should contain at least one digit`, () => {
+      const invalidMail
+        = validateRegisterForm('.test@mail.com', 'P@ssword1!');
+
+      expect(invalidMail.message).toBe('Email is invalid.');
+    });
+
+    it(`should contain '@' symbol`, () => {
+      const invalidMail
+        = validateRegisterForm('testmail.com', 'P@ssword1!');
+
+      expect(invalidMail.message).toBe('Email is invalid.');
+    });
+
+    it(
+      `should contain correct top-level domain (gmail.com, diia.org etc)`,
+      () => {
+        const invalidMail
+          = validateRegisterForm('test@gmail.c', 'P@ssword1!');
+
+        expect(invalidMail.message).toBe('Email is invalid.');
+      });
+
+    it(
+      `should not start with '@' symbol `,
+      () => {
+        const invalidMail
+            = validateRegisterForm('@mail.com', 'P@ssword1!');
+
+        expect(invalidMail.message).toBe('Email is invalid.');
+      });
+
+    it(`should not start with dot`, () => {
+      const invalidMail
+        = validateRegisterForm('.test@mail.com', 'P@ssword1!');
+
+      expect(invalidMail.message).toBe('Email is invalid.');
+    });
+
+    it(`should not end with dot`, () => {
+      const invalidMail
+        = validateRegisterForm('test@mail.com.', 'P@ssword1!');
+
+      expect(invalidMail.message).toBe('Email is invalid.');
+    });
+
+    it(`should not contain double dots`, () => {
+      const invalidMail
+        = validateRegisterForm('test@mail..com', 'P@ssword1!');
+
+      expect(invalidMail.message).toBe('Email is invalid.');
+    });
+  });
+
+  describe(`Password field`, () => {
+    it(`should be at least 8 characters length`, () => {
+      const invalidPassword = validateRegisterForm('test@mail.com', 'Q@c1');
+
+      expect(invalidPassword.message).toEqual('Password is invalid.');
+    });
+
+    it(`should be maximum 16 characters length`, () => {
+      const invalidPassword
+        = validateRegisterForm('test@mail.com', 'Q@c34567890qwerty');
+
+      expect(invalidPassword.message).toEqual('Password is invalid.');
+    });
+
+    it(`should contain at least one digit`, () => {
+      const invalidPassword = validateRegisterForm('test@mail.com', 'Q!ertyui');
+
+      expect(invalidPassword.message).toEqual('Password is invalid.');
+    });
+
+    it(`should contain at least one special character`, () => {
+      const invalidPassword = validateRegisterForm('test@mail.com', 'Qwertyu1');
+
+      expect(invalidPassword.message).toEqual('Password is invalid.');
+    });
+
+    it(`should contain at least one uppercase character`, () => {
+      const invalidPassword = validateRegisterForm('test@mail.com', 'qwerty1!');
+
+      expect(invalidPassword.message).toEqual('Password is invalid.');
+    });
+  });
 });
