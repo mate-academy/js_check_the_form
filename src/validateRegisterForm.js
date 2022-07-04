de66388b2f2d8c253ffd5ad49c235f7e07be0d29
krolich500grn <edu.izaytsev@gmail.com>
2022-07-04 14:24:12 +0300

    fix: regex email validation acc to task

--- src/validateRegisterForm.js
+++ src/validateRegisterForm.js
@@ -12,28 +12,32 @@ function validateRegisterForm(email, password) {
   const validPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,16}$/;
 
   // eslint-disable-next-line max-len
-  const validEmailMask = new RegExp(/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\./i);
+  const validEmailMask = new RegExp(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/);
 
   if (!email.match(validEmailMask) && password.match(validPassword)) {
     return {
-      code: 422, message: 'Email is invalid.',
+      code: 422,
+      message: 'Email is invalid.',
     };
   }
 
   if (email.match(validEmailMask) && !password.match(validPassword)) {
     return {
-      code: 422, message: 'Password is invalid.',
+      code: 422,
+      message: 'Password is invalid.',
     };
   }
 
   if (!email.match(validEmailMask) && !password.match(validPassword)) {
     return {
-      code: 500, message: 'Password and email are invalid.',
+      code: 500,
+      message: 'Password and email are invalid.',
     };
   }
 
   return {
-    code: 200, message: 'Email and password are valid.',
+    code: 200,
+    message: 'Email and password are valid.',
   };
 }
 
