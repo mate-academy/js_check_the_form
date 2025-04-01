41054a9efb3ca8cb444c9c5c9bd6c86c8903825d
WalentynaSadowa <walentynasadowa28@gmail.com>
2025-04-01 10:06:41 +0200

    check_the_form

--- src/validateRegisterForm.test.js
+++ src/validateRegisterForm.test.js
@@ -86,7 +86,8 @@ describe(`Function 'validateRegisterForm':`, () => {
 
   it(`should return error for email with a top-level domain starting with '.'`,
     () => {
-      const invalidEmail = validateRegisterForm('test@mail..com', 'P@ssword1!');
+      const invalidEmail = validateRegisterForm('.test@mail..com',
+        'P@ssword1!');
 
       expect(invalidEmail.code).toBe(422);
       expect(invalidEmail.message).toBe('Email is invalid.');
